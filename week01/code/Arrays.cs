468109cef929b96629e16d5e205aa29b75bcef45
Secret-Doughnut <dustinorangecook@gmail.com>
2026-03-07 14:07:46 -0700

    Tried to solve the second problem, but failed.

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -39,9 +39,47 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create a new list and a Counter variable.
+        // List<int> Numbers = new List<int>();
+        // int Counter = 1;
+
+        // // Create a foreach loop that goes through all of the items in the list named "data".
+        // // Have an if statement, that asks if the Counter variable is the same as the "amount" variable.
+        // // Inside of the if statement, add the numbers that enter it, into the new list created above.
+        // foreach (int number in data)
+        // {
+        //     if (Counter >= amount)
+        //     {
+        //         Numbers.Add(number);
+        //     }
+
+        //     Counter += 1;
+        // }
+
+        // Counter = 1;
+
+        // //  Have another foreach loop that goes through "data".
+        // // This loop has an if statement that grabs all of the numbers LOWER then "amount".
+        // // And adds those to the same list.
+        // foreach (int thing in data)
+        // {
+        //     if (Counter < amount)
+        //     {
+        //         Numbers.Add(thing);
+        //     }
+
+        //     Counter += 1;
+        // }
+
+        // // Clear out the old list, so we can copy the new list order back into it.
+        // data.Clear();
+
+
+        // // Use a foreach loop to copy the data from the new list and transfer it to the "data" list.
+        // foreach (int item in Numbers)
+        // {
+        //     data.Add(item);
+        // }
+
     }
 }
