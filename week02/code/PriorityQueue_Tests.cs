fdb9124335fcfc9fdada82dd9820218eaa220db3
Secret-Doughnut <dustinorangecook@gmail.com>
2026-03-11 19:43:41 -0600

    I think I have finally completed the test thing homework!

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,3 +1,5 @@
+using System.Configuration.Assemblies;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 // TODO Problem 2 - Write and run test cases and fix the code to match requirements.
@@ -6,23 +8,70 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with these strings with the priority in parentheses: Fire(4), Work(3), Tim(2), and Pizza(2).
+    // Expected Result: Pizza, Tim, Work, Fire.
+    // Defect(s) Found: I have no idea if I wrote this correctly, but the order of the enqeues
+    // are not in the right order. It says that the first item is different then expected.
     public void TestPriorityQueue_1()
     {
+        List<string> Test_List = new();
+        List<string> Expected_List = new List<string> {"Pizza", "Tim", "Work", "Fire"};
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Pizza", 2);
+        priorityQueue.Enqueue("Tim", 2);
+        priorityQueue.Enqueue("Work", 3);
+        priorityQueue.Enqueue("Fire", 4);
+
+
+        int Counter = 4;
+
+        while (Counter != 0)
+        {
+            string Number = priorityQueue.Dequeue();
+            Test_List.Add(Number);
+            Counter -= 1;
+        }
+
+        CollectionAssert.AreEqual(Test_List, Expected_List);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with these strings with the priority in parentheses: First(10), Second(2),
+    // Third(1), Fourth(7), Fifth(9), Sixth(8), Seventh(6), Eighth(5), Ninth(4), Tenth(3).
+    // Expected Result: Third, Second, Tenth, Ninth, Eighth, Seventh, Fourth, Sixth, Fifth, First.
+    // Defect(s) Found: I have no idea if I wrote this correctly, but the order of the enqeues 
+    // are not in the right order.
     public void TestPriorityQueue_2()
     {
+        List<string> Test_List = new();
+        List<string> Expected_List = new List<string> {"Third", "Second", "Tenth", "Ninth", "Eighth", "Seventh", "Fourth", "Sixth", "Fifth", "First"};
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("First", 10);
+        priorityQueue.Enqueue("Second", 2);
+        priorityQueue.Enqueue("Third", 1);
+        priorityQueue.Enqueue("Fourth", 7);
+        priorityQueue.Enqueue("Fifth", 9);
+        priorityQueue.Enqueue("Sixth", 8);
+        priorityQueue.Enqueue("Seventh", 6);
+        priorityQueue.Enqueue("Eighth", 5);
+        priorityQueue.Enqueue("Ninth", 4);
+        priorityQueue.Enqueue("Tenth", 3);
+
+
+        int Counter = 10;
+
+        while (Counter != 0)
+        {
+            string Numberth = priorityQueue.Dequeue();
+            Test_List.Add(Numberth);
+            Counter -= 1;
+        }
+
+        CollectionAssert.AreEqual(Test_List, Expected_List);
     }
 
     // Add more test cases as needed below.
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: The order for the function seem to be broken. It looks like the code is putting
+    // Sue before Bob. 
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +44,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: Looks like the ordering of the people are mixed up. It wants Sue 
+    // before Bob, even though he should have the first turn.
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +87,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: I think the code isn't tracking the infinite turns well.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +118,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: Looks like the code is not taking into consideration that Tim has 
+    // infinite turns.
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
@@ -143,7 +146,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: This part of the code seem to be working pretty well!
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
