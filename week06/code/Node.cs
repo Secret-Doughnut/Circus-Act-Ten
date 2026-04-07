faa9308cad5fefc059ad1a220adc56fd9c6bddb7
Secret-Doughnut <dustinorangecook@gmail.com>
2026-04-07 10:35:56 -0600

    Solved three out of five problems so far.

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,28 +13,64 @@ public class Node
     {
         // TODO Start Problem 1
 
-        if (value < Data)
+
+        if (value != Data)
         {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
         else
         {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
+            Console.WriteLine("You already have that value!");
+            return;
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        
+        bool Thing = false;
+
+        if (value < Data && Left is not null)
+        {
+            // Told this to not worry about it being null.
+            // Since I don't think a null value,
+            // can pass the if statement.
+            Thing = Left!.Contains(value);
+        }
+        else if (value > Data && Right is not null)
+        {
+            // Told this to not worry about it being null.
+            // Since I don't think a null value,
+            // can pass the if statement.
+            Thing = Right!.Contains(value);
+        }
+        else if (value == Data)
+        {
+            return Thing = true;
+        }
+        else
+        {
+            Console.WriteLine("Can't find a match to the value requested.");
+            return false;
+        }
+
+        return Thing;
     }
 
     public int GetHeight()
